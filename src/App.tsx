fb9d570dd001e2568aee14f965762f9ea67f4676
So Mei Amy Lam <lamsomeiamy@gmail.com>
2025-04-20 21:39:10 -0700

    Added Layout to App

--- src/App.tsx
+++ src/App.tsx
@@ -1,40 +1,21 @@
-import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
 import TopicForm from './pages/TopicForm'
 import Topics from './pages/Topics'
 import Topic from './pages/Topic'
-import Summary from './pages/Summary'
+import AllPosts from './pages/AllPosts'
+import { Layout } from './components/Layout'
 
 function App() {
   return (
     <Router>
-      <div className="flex space-x-6">
-        <Link
-          to="/"
-          className="hover:text-gray-300 transition-colors duration-300"
-        >
-          Home
-        </Link>
-        <Link
-          to="/topics"
-          className="hover:text-gray-300 transition-colors duration-300"
-        >
-          Topics
-        </Link>
-        <Link
-          to="/create-topic"
-          className="hover:text-gray-300 transition-colors duration-300"
-        >
-          Create Topic
-        </Link>
-      </div>
-      <div className="max-w-5xl flex flex-col justify-center items-center p-20 gap-14">
+      <Layout>
         <Routes>
           <Route path="/topics" element={<Topics />} />
           <Route path="/create-topic" element={<TopicForm />} />
           <Route path="/topics/:id" element={<Topic />} />
-          <Route path="/" element={<Summary />} />
+          <Route path="/" element={<AllPosts />} />
         </Routes>
-      </div>
+      </Layout>
     </Router>
   )
 }
